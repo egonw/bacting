b9f3e87ce7ec9c3ab338f7c9ad447fa91fb8e6ad
Egon Willighagen <egon.willighagen@gmail.com>
2019-04-12 07:31:19 +0200

    Added the fromSMILES() method

--- bioclipse-cdk/src/main/java/net/bioclipse/managers/CDKManager.java
+++ bioclipse-cdk/src/main/java/net/bioclipse/managers/CDKManager.java
@@ -15,7 +15,14 @@ package net.bioclipse.managers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.openscience.cdk.exception.InvalidSmilesException;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.silent.SilentChemObjectBuilder;
+import org.openscience.cdk.smiles.SmilesParser;
+
+import net.bioclipse.cdk.domain.CDKMolecule;
 import net.bioclipse.cdk.domain.ICDKMolecule;
+import net.bioclipse.core.business.BioclipseException;
 
 public class CDKManager {
 
@@ -29,5 +36,18 @@ public class CDKManager {
 	public List<ICDKMolecule> createMoleculeList() {
 		return new ArrayList<ICDKMolecule>();
 	}
+
+    public ICDKMolecule fromSMILES(String smilesDescription)
+            throws BioclipseException {
+        SmilesParser parser = new SmilesParser( SilentChemObjectBuilder.getInstance() );
+        IAtomContainer molecule;
+        try {
+            molecule = parser.parseSmiles( smilesDescription.trim() );
+        } catch (InvalidSmilesException e) {
+            String message = "SMILES string is invalid. Error message said: ";
+            throw new BioclipseException( message + e.getMessage(), e );
+        }
+        return new CDKMolecule(molecule);
+    }
 
 }
