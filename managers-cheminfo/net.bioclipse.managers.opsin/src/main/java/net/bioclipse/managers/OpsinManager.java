eaa157db4729e78100db193362f05c29481524b4
Egon Willighagen <egon.willighagen@gmail.com>
2019-04-20 10:38:09 +0200

    Added the OPSIN manager

---
+++ managers-cheminfo/net.bioclipse.managers.opsin/src/main/java/net/bioclipse/managers/OpsinManager.java
@@ -0,0 +1,74 @@
+/* Copyright (c) 2010,2019  Egon Willighagen <egon.willighagen@gmail.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contact: http://www.bioclipse.net/
+ */
+package net.bioclipse.managers;
+
+import net.bioclipse.cdk.domain.ICDKMolecule;
+import net.bioclipse.core.business.BioclipseException;
+import uk.ac.cam.ch.wwmm.opsin.NameToStructure;
+import uk.ac.cam.ch.wwmm.opsin.NameToStructureException;
+import uk.ac.cam.ch.wwmm.opsin.OpsinResult;
+import uk.ac.cam.ch.wwmm.opsin.OpsinResult.OPSIN_RESULT_STATUS;
+
+public class OpsinManager {
+
+	private CDKManager cdk;
+
+	public OpsinManager(String workspaceRoot) {
+		this.cdk = new CDKManager(workspaceRoot);
+	}
+
+	public ICDKMolecule parseIUPACName(String iupacName) 
+	                    throws BioclipseException {
+		return cdk.fromSMILES(parseIUPACNameAsSMILES(iupacName));
+	}
+
+	public String parseIUPACNameAsCML(String iupacName) 
+	              throws BioclipseException {
+		NameToStructure nameToStructure;
+		try {
+			nameToStructure = NameToStructure.getInstance();
+		} catch (NameToStructureException e) {
+			throw new BioclipseException(
+				"Error while loading OPSIN: " + e.getMessage(),
+				e
+			);
+		}
+        OpsinResult result = nameToStructure.parseChemicalName(iupacName);
+        if (result.getStatus() == OPSIN_RESULT_STATUS.SUCCESS) {
+        	return result.getCml();
+        }
+        throw new BioclipseException(
+        	"Could not parse the IUPAC name (" + iupacName + "), because: " +
+        	result.getMessage()
+        );
+	}
+
+    public String parseIUPACNameAsSMILES(String iupacName) 
+                  throws BioclipseException {
+    	NameToStructure nameToStructure;
+		try {
+			nameToStructure = NameToStructure.getInstance();
+		} catch (NameToStructureException e) {
+			throw new BioclipseException(
+				"Error while loading OPSIN: " + e.getMessage(),
+				e
+			);
+		}
+        OpsinResult result = nameToStructure.parseChemicalName(iupacName);
+        if (result.getStatus() == OPSIN_RESULT_STATUS.SUCCESS) {
+        	return result.getSmiles();
+        }
+        throw new BioclipseException(
+        	"Could not parse the IUPAC name (" + iupacName + "), because: " +
+        	result.getMessage()
+        );
+    }
+
+}
