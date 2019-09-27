3b0c981881d85e7b79a47ac609711b9827166098
Egon Willighagen <egon.willighagen@gmail.com>
2019-09-27 12:13:26 +0200

    Added an interface for the managers

---
+++ io.github.egonw.bacting/src/main/java/io/github/egonw/bacting/IBactingManager.java
@@ -0,0 +1,23 @@
+/* Copyright (c) 2019  Egon Willighagen <egonw@users.sf.net>
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package io.github.egonw.bacting;
+
+import java.util.List;
+
+import net.bioclipse.managers.business.IBioclipseManager;
+
+public interface IBactingManager extends IBioclipseManager {
+
+    /**
+     * Lists the DOIs of the articles associated to this manager.
+     *
+     * @return a {@link List} of String with DOIs
+     */
+    public List<String> doi();
+
+}
