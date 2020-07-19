91aadfd0dc2ec768d3b2c1b097683c47a1508d13
Egon Willighagen <egon.willighagen@gmail.com>
2020-07-19 09:29:03 +0200

    Added missing copyright notices

--- managers-cheminfo/net.bioclipse.managers.cdk/src/test/java/net/bioclipse/managers/CDKManagerTest.java
+++ managers-cheminfo/net.bioclipse.managers.cdk/src/test/java/net/bioclipse/managers/CDKManagerTest.java
@@ -1,3 +1,12 @@
+/* Copyright (c) 2020  Egon Willighagen <egon.willighagen@gmail.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contact: http://www.bioclipse.net/
+ */
 package net.bioclipse.managers;
 
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
