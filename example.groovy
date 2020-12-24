9708822f818c839cc8285b2d03dc653f93a221c3
Egon Willighagen <egon.willighagen@gmail.com>
2020-12-24 18:55:08 +0100

    Removed a very old, no longer working example

--- example.groovy
+++
@@ -1,9 +0,0 @@
-@Grab(group='net.bioclipse.bacting', module='managers-cdk', version='0.0.3-SNAPSHOT')
-
-workspaceRoot = "."
-def cdk = new net.bioclipse.managers.CDKManager(workspaceRoot);
-
-list = cdk.createMoleculeList()
-println list
-println cdk.fromSMILES("COC")
-
