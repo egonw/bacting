6b375616f0a5597e23ee1086c2cf1267ed51e08a
Egon Willighagen <egon.willighagen@gmail.com>
2021-07-03 15:04:46 +0200

    Added a test for reading an Excel spreadsheet

--- managers-excel/src/test/java/net/bioclipse/managers/ExcelManagerTest.java
+++ managers-excel/src/test/java/net/bioclipse/managers/ExcelManagerTest.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2020  Egon Willighagen <egon.willighagen@gmail.com>
+/* Copyright (c) 2020-2021  Egon Willighagen <egon.willighagen@gmail.com>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -12,21 +12,31 @@ package net.bioclipse.managers;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import net.bioclipse.core.business.BioclipseException;
+import net.bioclipse.core.domain.StringMatrix;
+
 public class ExcelManagerTest {
 
 	static ExcelManager excel;
+	static UIManager ui;
 	static String workspaceRoot;
 
 	@BeforeAll
 	static void setupManager() throws Exception {
 		workspaceRoot = Files.createTempDirectory("exceltestws").toString();
 		excel = new ExcelManager(workspaceRoot);
+		ui = new UIManager(workspaceRoot);
+		InputStream stream = ExcelManagerTest.class.getClassLoader().getResourceAsStream("testSpreadsheet.xlsx");
+		ui.newProject("/NewFiles/");
+		String newFile = ui.newFile("/NewFiles/testSpreadsheet.xlsx");
+		ui.append(newFile, stream);
 	}
 
 	@Test
@@ -34,5 +44,11 @@ public class ExcelManagerTest {
 		List<String> dois = excel.doi();
 		assertNotNull(dois);
 		assertSame(0, dois.size());
+	}
+
+	@Test
+	public void testGetSheet() throws BioclipseException {
+		StringMatrix sheet = excel.getSheet("/NewFiles/testSpreadsheet.xlsx", 0);
+		assertNotNull(sheet);
 	}
 }
