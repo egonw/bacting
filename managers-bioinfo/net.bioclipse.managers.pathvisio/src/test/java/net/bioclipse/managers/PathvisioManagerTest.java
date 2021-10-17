c3b889b15b4800349ac36c3790b872c6f3d0d9df
Egon Willighagen <egon.willighagen@gmail.com>
2021-10-17 15:04:32 +0200

    A few more tests, for better coverage

--- managers-bioinfo/net.bioclipse.managers.pathvisio/src/test/java/net/bioclipse/managers/PathvisioManagerTest.java
+++ managers-bioinfo/net.bioclipse.managers.pathvisio/src/test/java/net/bioclipse/managers/PathvisioManagerTest.java
@@ -23,23 +23,40 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.pathvisio.model.PathwayModel;
 
-import net.bioclipse.core.business.BioclipseException;
-
 public class PathvisioManagerTest {
 
 	static PathvisioManager pathvisio;
+	static UIManager ui;
 
 	@BeforeAll
 	static void setupManager() throws IOException {
 		String tmpPath = Files.createTempDirectory("pvtestws").toString();
 		pathvisio = new PathvisioManager(tmpPath);
+		ui = new UIManager(tmpPath);
 	}
 
 	@Test
-	public void testGetGPML() throws BioclipseException {
+	public void testGetGPML() throws Exception {
 		String gpmlFile = pathvisio.getGPML("WP4846");
 		assertNotNull(gpmlFile);
 		assertTrue(gpmlFile.contains("WP4846.gpml"));
+		ui.renewFile(gpmlFile);
+	}
+
+	@Test
+	public void testGetGPML_JustNumber() throws Exception {
+		String gpmlFile = pathvisio.getGPML("4846");
+		assertNotNull(gpmlFile);
+		assertTrue(gpmlFile.contains("WP4846.gpml"));
+		ui.renewFile(gpmlFile);
+	}
+
+	@Test
+	public void testGetGPML_WithRevision() throws Exception {
+		String gpmlFile = pathvisio.getGPML("WP4846_r114246");
+		assertNotNull(gpmlFile);
+		assertTrue(gpmlFile.contains("WP4846_r114246.gpml"));
+		ui.renewFile(gpmlFile);
 	}
 
 	@Test
@@ -55,6 +72,13 @@ public class PathvisioManagerTest {
 		Set<String> hits = pathvisio.queryWikipathways("orf1");
 		assertNotNull(hits);
 		assertNotEquals(0, hits.size());
+	}
+
+	@Test
+	public void queryWikipathways_NotAGene() throws Exception {
+		Set<String> hits = pathvisio.queryWikipathways("SARS-CoV-2");
+		assertNotNull(hits);
+		assertSame(0, hits.size());
 	}
 
 	@Test
