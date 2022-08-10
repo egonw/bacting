473bd3e541d0bcdda005938e7f49bf36ab32579e
Egon Willighagen <egon.willighagen@gmail.com>
2022-08-10 15:01:56 +0200

    Added a methods to help generate nanoInChIs

--- managers-cheminfo/net.bioclipse.managers.nm/src/main/java/net/bioclipse/managers/NanoManager.java
+++ managers-cheminfo/net.bioclipse.managers.nm/src/main/java/net/bioclipse/managers/NanoManager.java
@@ -18,11 +18,11 @@ import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.core.runtime.CoreException;
-import org.openscience.cdk.silent.SilentChemObjectBuilder;
-import org.openscience.cdk.tools.manipulator.MolecularFormulaManipulator;
+import org.openscience.cdk.exception.CDKException;
 
 import io.github.egonw.bacting.IBactingManager;
 import io.github.egonw.nanojava.data.MaterialType;
+import io.github.egonw.nanojava.inchi.NInChIGenerator;
 import net.bioclipse.core.business.BioclipseException;
 import net.bioclipse.managers.domain.IMaterial;
 import net.bioclipse.managers.domain.Material;
@@ -62,18 +62,42 @@ public class NanoManager implements IBactingManager {
     	return new Material(material);
     }
 
-    public IMaterial newMaterial(String type, String formula) throws BioclipseException {
+    public IMaterial newMaterial(String type, String label) throws BioclipseException {
     	Material material = (Material)newMaterial(type);
-    	io.github.egonw.nanojava.data.Material nm = material.getInternalModel();
-    	nm.addAtomContainer(MolecularFormulaManipulator.getAtomContainer(
-    		MolecularFormulaManipulator.getMolecularFormula(formula, SilentChemObjectBuilder.getInstance())
-    	));
-    	return material;
+        List<String> labels = new ArrayList<>(); labels.add(label);
+        material.getInternalModel().setLabels(labels);
+        return material;
+    }
+
+    public IMaterial newMaterial(io.github.egonw.nanojava.data.Material material) throws BioclipseException {
+        return new Material(material);
     }
 
     public Set<String> listMaterialTypes()
     		throws BioclipseException, UnsupportedEncodingException, CoreException {
     	return Collections.unmodifiableSet(materialTypes.keySet());
+    }
+
+    public String generateInChI(IMaterial material) throws BioclipseException {
+        if (material instanceof Material) {
+            io.github.egonw.nanojava.data.Material nm = ((Material)material).getInternalModel();
+            if (nm.getAtomContainerCount() == 0) {
+                throw new BioclipseException(
+                    "Cannot generate NanoInChIs for materials without at least one chemical composition"
+                );
+            }
+            try {
+                return NInChIGenerator.generator(nm);
+            } catch (CDKException exception) {
+                throw new BioclipseException(
+                    "Error while generating the NanoInChI: " + exception.getMessage(), exception
+                );
+		    }
+        } else {
+            throw new BioclipseException(
+                "Unsupported IMaterial implementation."
+            );
+        }
     }
 
 	@Override
--- managers-cheminfo/net.bioclipse.managers.nm/src/test/java/net/bioclipse/managers/NanoManagerTest.java
+++ managers-cheminfo/net.bioclipse.managers.nm/src/test/java/net/bioclipse/managers/NanoManagerTest.java
@@ -11,12 +11,12 @@ package net.bioclipse.managers;
 
 import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.ByteArrayInputStream;
 import java.nio.file.Files;
 import java.util.List;
 import java.util.Set;
@@ -25,7 +25,13 @@ import org.junit.Assert;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import com.github.jqudt.onto.units.LengthUnit;
+
+import io.github.egonw.nanojava.data.Material;
+import io.github.egonw.nanojava.data.MaterialBuilder;
 import io.github.egonw.nanojava.data.MaterialType;
+import io.github.egonw.nanojava.data.measurement.EndPoints;
+import io.github.egonw.nanojava.data.measurement.ErrorlessMeasurementValue;
 import net.bioclipse.core.business.BioclipseException;
 import net.bioclipse.managers.domain.IMaterial;
 
@@ -58,7 +64,7 @@ public class NanoManagerTest {
 	public void testNewMaterialWithNullType() throws BioclipseException {
 		Exception exception = assertThrows(BioclipseException.class, () ->
 		{
-			nm.newMaterial(null);
+			nm.newMaterial((String)null);
 		});
 		assertTrue(exception.getMessage().contains("Unknown material type"));
 	}
@@ -85,7 +91,29 @@ public class NanoManagerTest {
 		Assert.assertNotNull(types);
 		assertNotSame(0, types.size());
 	}
-	
+
+	@Test
+	public void testGenerateInChI_InsufficientChemicalInfo() throws Exception {
+		Exception exception = assertThrows(BioclipseException.class, () ->
+		{
+			IMaterial material = nm.newMaterial("metal oxide", "TiO2");
+			nm.generateInChI(material);
+		});
+		assertTrue(exception.getMessage().contains("without at least one chemical composition"));
+	}
+
+	@Test
+	public void testGenerateInChI() throws Exception {
+		Material material = MaterialBuilder.type("METALOXIDE")
+			.label("silica nanoparticles with gold coating")
+			.componentFromSMILES(1, "O=[Si]=O", "SPHERE", "AMORPHOUS", new ErrorlessMeasurementValue(EndPoints.DIAMETER, 20, LengthUnit.NM))
+			.componentFromSMILES(2, "[Au]", "SHELL", new ErrorlessMeasurementValue(EndPoints.THICKNESS, 2, LengthUnit.NM))
+			.asMaterial();
+		IMaterial iMaterial = nm.newMaterial(material);
+		String inchi = nm.generateInChI(iMaterial);
+		assertEquals("NInChI=0.00.1A/Au/msh/s2t-9!O2Si/c1-3-2/msp/s20d-9/k000/y2&1", inchi);
+	}
+
 	@Test
 	public void testDOIs() {
 		List<String> dois = nm.doi();
