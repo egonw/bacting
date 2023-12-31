fbbb6a5b93368fdd736310795adbfbd058f4543c
Egon Willighagen <egon.willighagen@gmail.com>
2023-12-31 09:11:53 +0100

    Added missing JavaDoc

--- managers-core/managers-jsoup/src/main/java/net/bioclipse/managers/JSoupManager.java
+++ managers-core/managers-jsoup/src/main/java/net/bioclipse/managers/JSoupManager.java
@@ -20,6 +20,9 @@ import org.jsoup.select.Elements;
 import io.github.egonw.bacting.IBactingManager;
 import net.bioclipse.core.business.BioclipseException;
 
+/**
+ * Manager for JSoup functionality to parse HTML content.
+ */
 public class JSoupManager implements IBactingManager {
 
 	private String workspaceRoot;
@@ -35,20 +38,46 @@ public class JSoupManager implements IBactingManager {
 		ui = new UIManager(workspaceRoot);
 	}
 
+    /**
+     * Parses a string with HTML content into the JSoup {@link org.jsoup.nodes.Document}.
+     *
+     * @param htmlString the HTML as {@link String}
+     * @return           the HTML content as {@link org.jsoup.nodes.Document}
+     */
     public Document parseString(String htmlString) {
     	Document doc = Jsoup.parse(htmlString);
     	return doc;
     };
 
+    /**
+     * Parses a file with HTML content from the workspace into the JSoup {@link org.jsoup.nodes.Document}.
+     *
+     * @param  htmlFile           the name of the HTML file in the workspace
+     * @return                    the HTML content as {@link org.jsoup.nodes.Document}
+     * @throws BioclipseException when the file could not be read
+     */
     public Document parse(String htmlFile) throws BioclipseException {
         String htmlString = ui.readFile(htmlFile);
         return parseString(htmlString);
     };
 
+    /**
+     * Takes a HTML string and removes all tags.
+     *
+     * @param htmlString the HTML as {@link String}
+     * @return           the text bits from the HTML
+     */
     public String removeHTMLTags(String htmlString) {
     	return Jsoup.parse(htmlString).text();
     };
 
+    /**
+     * Selects a subsection of the {@link Document} and returns it as an {@link Elements} object.
+     *
+     * @param doc         JSoup document to select from as {@link Element}
+     * @param cssSelector String with a Cascading Style Sheet selector instruction
+     * @return            the selected content
+     */
     public Elements select(Element doc, String cssSelector) {
     	Elements results = doc.select(cssSelector);
     	return results;    	
