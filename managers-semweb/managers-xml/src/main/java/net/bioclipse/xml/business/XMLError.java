a3b37c110743fc7122768a6a123c9e6bad1d97c9
Egon Willighagen <egon.willighagen@gmail.com>
2024-02-18 15:46:49 +0100

    Added missing JavaDoc

--- managers-semweb/managers-xml/src/main/java/net/bioclipse/xml/business/XMLError.java
+++ managers-semweb/managers-xml/src/main/java/net/bioclipse/xml/business/XMLError.java
@@ -10,22 +10,41 @@
  ******************************************************************************/
 package net.bioclipse.xml.business;
 
+/**
+ * Error that happens during validation of an XML file.
+ */
 public class XMLError {
 
     private int lineNumber = -1;
     private int columnNumber = -1;
     private String message;
 
+    /**
+     * Creates a new XML validation error with the given message.
+     *
+     * @param message description of the validation error
+     */
     public XMLError(String message) {
         this.message = message;
     }
 
+    /**
+     * Creates a new XML validation error with the given message.
+     *
+     * @param message description of the validation error
+     * @param lineNumber    line at what the validation error is location
+     * @param columnNumber  position on the line where the validation error is location
+     */
     public XMLError(String message, int lineNumber, int columnNumber) {
         this.message = message;
         this.lineNumber = lineNumber;
         this.columnNumber = columnNumber;
     }
 
+    /**
+     * Generates a representation of the full validation error information.
+     * It includes the message and, if given, the location in the file.
+     */
     public String toString() {
         if (lineNumber != -1 && columnNumber != -1)
             return "l" + lineNumber + ",c" + columnNumber 
