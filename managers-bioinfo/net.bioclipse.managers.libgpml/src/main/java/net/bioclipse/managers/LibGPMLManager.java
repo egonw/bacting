665c7ec4b8b3fc76eca55052c1bc305fb7c2c183
Egon Willighagen <egon.willighagen@gmail.com>
2024-08-30 10:36:02 +0200

    Added first generation load/save methods

--- managers-bioinfo/net.bioclipse.managers.libgpml/src/main/java/net/bioclipse/managers/LibGPMLManager.java
+++ managers-bioinfo/net.bioclipse.managers.libgpml/src/main/java/net/bioclipse/managers/LibGPMLManager.java
@@ -9,10 +9,20 @@
  */
 package net.bioclipse.managers;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.util.Collections;
 import java.util.List;
 
+import org.pathvisio.libgpml.io.ConverterException;
+import org.pathvisio.libgpml.model.GPML2013aWriter;
+import org.pathvisio.libgpml.model.Pathway;
+import org.pathvisio.libgpml.model.PathwayModel;
+
 import io.github.egonw.bacting.IBactingManager;
+import net.bioclipse.core.business.BioclipseException;
 
 /**
  * Bioclipse manager that provides BioJava functionality.
@@ -30,6 +40,33 @@ public class LibGPMLManager implements IBactingManager {
 		this.workspaceRoot = workspaceRoot;
 	}
 
+	public PathwayModel loadModel(String file) throws BioclipseException {
+		File fileObj = new File(workspaceRoot + file);
+		PathwayModel pathwayModel = new PathwayModel();
+		try {
+			pathwayModel.readFromXml(fileObj, true);
+		} catch (ConverterException exception) {
+			throw new BioclipseException("Error while reading GPML file: " + exception.getMessage(), exception);
+		}
+		return pathwayModel;
+	}
+
+	/**
+	 * Save a GPML model in GPML2013a file format.
+	 *
+	 * @param file                the file to save too
+	 * @param pathwayModel        the GPML model to save
+	 * @throws BioclipseException
+	 */
+	public void saveModelAsGPML2013a(String file, PathwayModel pathwayModel) throws BioclipseException {
+		File fileObj = new File(workspaceRoot + file);
+    	try {
+			GPML2013aWriter.GPML2013aWRITER.writeToXml(pathwayModel, fileObj, true);
+		} catch (ConverterException exception) {
+			throw new BioclipseException("Error while reading GPML file: " + exception.getMessage(), exception);
+		}
+	}
+	
 	@Override
 	public String getManagerName() {
 		return "gpml";
--- managers-bioinfo/net.bioclipse.managers.libgpml/src/test/java/net/bioclipse/managers/LibGPMLManagerTest.java
+++ managers-bioinfo/net.bioclipse.managers.libgpml/src/test/java/net/bioclipse/managers/LibGPMLManagerTest.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2020  Egon Willighagen <egon.willighagen@gmail.com>
+/* Copyright (c) 2020,2024  Egon Willighagen <egon.willighagen@gmail.com>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -13,20 +13,41 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.pathvisio.libgpml.model.PathwayModel;
 
 public class LibGPMLManagerTest {
 
 	static LibGPMLManager gpml;
+	static UIManager ui;
 
 	@BeforeAll
 	static void setupManager() throws IOException {
 		String tmpPath = Files.createTempDirectory("libgpmltestws").toString();
 		gpml = new LibGPMLManager(tmpPath);
+		ui = new UIManager(tmpPath);
+		ui.newProject("/GPMLTests/");
+
+		InputStream stream = LibGPMLManagerTest.class.getClassLoader().getResourceAsStream("WP1546.gpml");
+		String newFile = ui.newFile("/GPMLTests/WP1546.gpml");
+		ui.append(newFile, stream);
+	}
+
+	@Test
+	public void testLoading() throws Exception {
+		PathwayModel pathway = gpml.loadModel("/GPMLTests/WP1546.gpml");
+		assertNotNull(pathway);
+	}
+
+	@Test
+	public void testSaving() throws Exception {
+		PathwayModel pathway = gpml.loadModel("/GPMLTests/WP1546.gpml");
+		gpml.saveModelAsGPML2013a("/GPMLTests/test.gpml", pathway);
 	}
 
 	@Test
