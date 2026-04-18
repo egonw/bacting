6b909c377a43b02eb693e8a2b423940bb63951fe
Egon Willighagen <egon.willighagen@gmail.com>
2026-04-18 22:41:40 +0200

    Use the same JUnit as the rest of Bacting

--- managers-semweb/net.bioclipse.managers.wikidata/src/test/java/net/bioclipse/managers/WikidataManagerTest.java
+++ managers-semweb/net.bioclipse.managers.wikidata/src/test/java/net/bioclipse/managers/WikidataManagerTest.java
@@ -9,10 +9,10 @@
  */
 package net.bioclipse.managers;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
--- managers-semweb/net.bioclipse.managers.wikidata/src/test/java/net/bioclipse/wikidata/domain/WikidataMoleculeTest.java
+++ managers-semweb/net.bioclipse.managers.wikidata/src/test/java/net/bioclipse/wikidata/domain/WikidataMoleculeTest.java
@@ -9,10 +9,10 @@
  */
 package net.bioclipse.wikidata.domain;
 
-import static org.junit.Assert.assertNotSame;
-import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
